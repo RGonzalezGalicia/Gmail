296e6d15037f384d8bdf488e3d642865596d02e3
Ricardo Gonzalez <ricardo_rlzda@outlook.com>
2023-07-27 16:52:26 -0600

    Celda personalizada

---
+++ PruebaStefanini/infoCell.swift
@@ -0,0 +1,23 @@
+//
+//  infoCell.swift
+//  PruebaStefanini
+//
+//  Created by Ricardo Gonzalez on 27/07/23.
+//
+
+import UIKit
+
+class infoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
